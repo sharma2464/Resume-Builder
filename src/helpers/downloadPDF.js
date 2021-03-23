a2768a913f369ffff3f4476813d8105f19b5391c
Manish S <manish.sharma2464@gmail.com>
2021-03-23 21:27:13 +0530

    Adds Redux Persist
    - Now Redux store rehydrates the data on page refresh

--- src/helpers/downloadPDF.js
+++ src/helpers/downloadPDF.js
@@ -1,6 +1,6 @@
 import html2canvas from "html2canvas";
 import {jsPDF} from "jspdf";
-import store from "../app/store";
+import {store} from "../app/store";
 
 // resumeContainer
 export default function handlePDFDownload(event) {
